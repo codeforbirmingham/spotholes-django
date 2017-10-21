1e246d1b84c3a21799e6269bdfce38d451912d93
casuru <casuru@uab.edu>
2017-10-21 10:41:36 +0000

    Updated and hid secret keys

--- spotholes/settings.py
+++ spotholes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(a9_qwbc&lo)3xo=m@+*%^2y3vc-civc9r3*)xt^ioj^o2op58'
+SECRET_KEY = os.environ['django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
